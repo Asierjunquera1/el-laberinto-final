3e072e411fcd7c3921b13e38871da9f0bd00eb20
Asierjunquera1 <ajunquerabartolome@gmail.com>
2022-12-08 23:51:18 +0100

    a

--- laberinto.py
+++ laberinto.py
@@ -5,30 +5,28 @@ laberinto = [
     [' ', ' ', ' ', 'X', ' '], 
     ['X', 'X', 'X', 'X', 'S']
     ]
-
 def recorrer_laberinto():
+    i=0
+    j=0
     lista=[]
-    posicion=laberinto[0][0]
-    for i in range(len(laberinto)-1):
-        posicion1=laberinto[1+i][0]
-        if posicion1==' ':
+    while laberinto[i][j]!='S':
+        if laberinto[i+1][j]==' ':
+            i+=1
             lista.append('abajo')
-        else:
-            for j in range(len(laberinto)-1):
-                posicion2=laberinto[i][j+1]
-                if posicion2==' ':
-                    lista.append('derecha')
- 
-                
-                    
-
     
-
-
-
-
+        elif laberinto[i][j+1]==' ':
+            j+=1
+            lista.append('derecha')
+    
+        elif laberinto[i-1][j]==' ':
+            i-=1
+            lista.append('arriba')
+    
+        elif laberinto[i][j-1]:
+            j-=1
+            lista.append('izquierda')
+        
 
     return lista
+
 print(recorrer_laberinto())
-
-
