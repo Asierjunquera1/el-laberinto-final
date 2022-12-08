0af8c0371aed3f628e2254570d4eee203a893fe0
Asierjunquera1 <ajunquerabartolome@gmail.com>
2022-12-09 00:13:46 +0100

    aaaa

--- laberinto.py
+++ laberinto.py
@@ -9,22 +9,27 @@ def recorrer_laberinto():
     i=0
     j=0
     lista=[]
+    lista2=[(0, 0)]
     while laberinto[i][j]!='S':
-        if laberinto[i+1][j]==' ':
+        if laberinto[i+1][j]==' ' and (i+1, j) not in lista2:
             i+=1
             lista.append('abajo')
+            lista2.append((i, j))
     
-        elif laberinto[i][j+1]==' ':
+        elif laberinto[i][j+1]==' ' and (i, j+1) not in lista2:
             j+=1
             lista.append('derecha')
-    
-        elif laberinto[i-1][j]==' ' and laberinto[i+1][j]=='X':
+            lista2.append((i, j))
+
+        elif laberinto[i-1][j]==' ' and (i-1, j) not in lista2:
             i-=1
             lista.append('arriba')
-    
-        elif laberinto[i][j-1] and laberinto[i][j-1]=='X':
+            lista2.append((i, j))
+            
+        elif laberinto[i][j-1]==' ' and (i, j-1) not in lista2:
             j-=1
             lista.append('izquierda')
+            lista2.append((i, j))
         
 
     return lista
